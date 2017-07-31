192507e11bc0277cade8808424c05486030978d0
matteorulli <matteo.rulli@gmail.com>
2017-07-31 11:18:52 +0200

    Improved users API

--- spring.jwt.example/src/main/java/spring/jwt/example/UserController.java
+++ spring.jwt.example/src/main/java/spring/jwt/example/UserController.java
@@ -1,16 +1,20 @@
 package spring.jwt.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import spring.jwt.example.model.User;
 
 @RestController
 public class UserController {
 
   /* Maps to all HTTP actions by default (GET,POST,..)*/
   @RequestMapping("/users")
-  public @ResponseBody String getUsers() {
-    return "{\"users\":[{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
-           "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}]}";
+  public @ResponseBody List<User> getUsers() {
+    return Arrays.asList(new User("John", "Deer"), new User("David", "Collins"));
   }
 }
