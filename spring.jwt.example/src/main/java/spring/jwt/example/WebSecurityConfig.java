41ed699b526150ff66a7a07bcbe175e793f210c2
matteorulli <matteo.rulli@gmail.com>
2017-07-31 11:22:57 +0200

    minor changes

--- spring.jwt.example/src/main/java/spring/jwt/example/WebSecurityConfig.java
+++ spring.jwt.example/src/main/java/spring/jwt/example/WebSecurityConfig.java
@@ -28,7 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests()
-		.antMatchers("/").permitAll()
+		.antMatchers("/").permitAll()	//comment this to get free access to the 'hello world' endpoint
 		.antMatchers(HttpMethod.POST, "/login").permitAll()
 		.anyRequest().authenticated()
 		.and()
